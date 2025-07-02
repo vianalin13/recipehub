d5d2b265797b07ee226cc9775e16a3dcd6216abe
vianalin13 <lin.viana13@gmail.com>
2025-07-02 04:12:01 -0400

    Fix vercel deployment again

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,9 +1,3 @@
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function Page({params}: PageProps) {
-  return <p>Recipe page for ID: {params.id}</p>
+export default function Page() {
+  return <p>Recipe Page</p>
 }
